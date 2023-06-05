f9113ba42f09bce0c04853ddd3b11c3dcdd1ecca
Petrov Kirill <ottokirik@gmail.com>
2023-06-06 01:23:11 +0300

    fix positive validator

--- src/schemes/number.scheme.js
+++ src/schemes/number.scheme.js
@@ -24,7 +24,8 @@ export default class NumberScheme extends CoreScheme {
     const isPositive = positive ? positive(value) : true;
     const isInRange = range ? range(value) : true;
 
-    console.log('value', value, isNumber && isPositive && isInRange && super.isValid(value) && isRequired);
+    console.log('+++++++++++++++', Object.keys(this.validators));
+    console.log('value', value, isNumber, isPositive, isInRange, super.isValid(value), isRequired);
 
     return isNumber && isPositive && isInRange && super.isValid(value) && isRequired;
   }
